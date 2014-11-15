224579c13b479918a7834bb7d2334f63e1989fdd
coffutt <coffutt@paypal.com>
2014-11-15 13:38:38 -0500

    Added in mongoose set up and configuration.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@
 var kraken = require('kraken-js'),
     app = require('express')(),
     options = require('./lib/spec')(app),
+    mongoose = require('mongoose');
     port = process.env.PORT || 8000;
 
+mongoose.connect('mongodb://localhost/scribe');
+var db = mongoose.connection;
 
-app.use(kraken(options));
-
-
-app.listen(port, function (err) {
-    console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  app.use(kraken(options));
+  app.listen(port, function (err) {
+      console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
+  });
 });
