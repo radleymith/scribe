56d253a3f0a8dbf51d691c4942a8302d349751cb
Barry Dutton <badutton@paypal.com>
2014-11-15 16:43:28 -0500

    Implemented upload page.

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,46 +1,70 @@
 'use strict';
 
-var async = require('async'),
+var pkg = require('../package.json'),
+    async = require('async'),
     LectureModel = require('../models/lecture'),
     transcriber = require('../lib/transcriber'),
     messaging = require('../lib/messaging');
 
-function initWaterfall (initParam) {
-    return function (callback) { callback(null, initParam); };
+function initWaterfall(initParam) {
+    return function(callback) {
+        callback(null, initParam);
+    };
 }
 
-function saveLectureMetadata (lectureId, callback) {
+function saveLectureMetadata(data, callback) {
     var lect = new LectureModel();
-    lect.courseId = req.body.lectureName || 'Undefined';
-    lect.uuid = lectureId;
-    lect.lectureDate = new Date();
+    lect.courseId = data.name || 'Undefined';
+    lect.uuid = data.id;
+    lect.lectureDate = data.date = new Date();
 
-    lect.save(function (err, res) {
-        callback(err, lectureId);
+    lect.save(function(err, res) {
+        callback(err, data);
     });
 }
 
-function sendEmail (lectureId, callback) {
-    var email = req.body.email,
-        url = 'http://localhost:8000/scribe/lectures/' + lectureId;
+function sendEmail(data, callback) {
+    var email = data.email,
+        url = 'http://localhost:8000/scribe/lectures/' + data.id;
 
     if (email) {
-        messaging.sendEmail(email, url, function (err, res) {
-            callback(err, lectureId);
+        messaging.sendEmail(email, url, function(err, res) {
+            callback(err, data);
         });
     } else {
-        callback(null, lectureId);
+        callback(null, data);
     }
 }
 
-module.exports.uploadVideo = function (req, res) {
+module.exports.uploadVideo = function(req, res) {
     async.waterfall([
         initWaterfall(req.files.video.path), // Inject the vid file path into waterfall
         transcriber.transcribe, // Transcribe the video
+        function(lectureId, callback) {
+            callback(null, {
+                id: lectureId,
+                name: req.body.vname,
+                email: req.body.email,
+                phone: req.body.phone,
+                description: req.body.description
+            });
+        },
         saveLectureMetadata, // Save the lecture metadata to mongo
-        sendEmail // Send a notification email to the user. 
-        ], function (err, lectureId) {
-            res.send(err || lectureId);
+        sendEmail // Send a notification email to the user.
+    ], function(err, data) {
+        if (err) {
+            res.send(err);
             res.end();
-        });
+        } else {
+            res.render('upload', {
+                lecture: {
+                    vname: data.name,
+                    guid: data.id,
+                    date: data.date,
+                    description: data.description
+                },
+                version: pkg.version
+            });
+        }
+    });
 };
