0c18e4e8c3ece2df1dc393301abf82d790e770dc
coffutt <coffutt@paypal.com>
2014-11-15 16:43:28 -0500

    Got table for lectures.

--- controllers/lectures.js
+++ controllers/lectures.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var LectureModel = require('../models/lecture');
+var LectureModel = require('../models/lecture'),
+    pkg = require('../package');
 
 module.exports.getAllLectures = function (req, res) {
     LectureModel.find(function (err, lectures) {
-        res.json(lectures);
+        var model = { version: pkg.version, model: { lectures: lectures }};
+        model.model.lectures.forEach(function (lecture) {
+            lecture.link = 'http://localhost:8000/scribe/lectures/' + lecture.uuid;
+        });
+        res.render('listLectures', model);
     });
 };
